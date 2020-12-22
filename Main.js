7d40b5b5c7fec8fe803fcbc2d36eb68ae3ce10b2
YouS4ckIt <youdimiit@gmail.com>
2020-12-22 04:45:43 +0100

    Using Api without the CLI anymore.

--- Main.js
+++ Main.js
@@ -2,34 +2,44 @@ require("dotenv").config();
 const fs = require("fs");
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const { exec } = require("child_process");
-
+const TikTokScraper = require("tiktok-scraper");
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
+let video = "video";
+let scrapperMsg;
+let options = {
+  _: ["video"],
+  d: true,
+  download: true,
+  asyncDownload: 5,
+  a: 5,
+  "async-download": 5,
+  filepath: "C:\\Users\\YouSuckIt\\Desktop\\DiscordBot",
+};
 
-client.on("message", (msg) => {
-  let pathToVideo;
+client.on("message", async (msg) => {
   if (msg.content.includes("https://vm.tiktok.com/")) {
-    let message = msg.content;
-    exec("tiktok-scraper video " + message + " -d", (error, stdout, stderr) => {
-      if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-      }
-      if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-      }
-      console.log(`stdout: ${stdout}`);
-      msg.delete();
-      let temp = `${stdout}`.split(" ");
-      pathToVideo = temp[2].split(/\n/g)[0];
-      const buffer = fs.readFileSync(String(pathToVideo));
-      const attachment = new Discord.MessageAttachment(buffer, "tester.mp4");
-      msg.reply(attachment);
-    });
+    var expression = /(https?:\/\/(?:www\.|(?!www))[^\s\.]+\.[^\s]{2,}|www\.[^\s]+\.[^\s]{2,})/gi;
+    let message = msg.content.match(expression);
+    await startScraper(message);
+    msg.delete();
+    const buffer = fs.readFileSync(String(scrapperMsg[2]));
+    const attachment = new Discord.MessageAttachment(buffer, "YourVideo.mp4");
+    msg.reply(attachment);
   }
 });
 
 client.login(process.env.BOTTOKEN);
+const startScraper = async (link) => {
+  try {
+    const scraper = await TikTokScraper[video](link, options);
+    if (scraper.message) {
+      console.log(scraper.message);
+      scrapperMsg = scraper.message.split(" ");
+    }
+  } catch (error) {
+    console.error(error.message);
+    console.error(error);
+  }
+};
